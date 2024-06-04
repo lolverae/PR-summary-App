5da1ca75f2f20a4350060da7c64c705897a95687
lolverae <alberto.olvera.espinosa@gmail.com>
2024-06-04 14:07:20 -0600

    Refactor email sending functionality in SendReport

--- services/email.go
+++ services/email.go
@@ -3,30 +3,33 @@ package services
 import (
 	"log"
 	"net/smtp"
+  "fmt"
 )
 
 func SendReport(report string) {
-	config, err := LoadConfig()
-	if err != nil {
-		log.Fatalf("Error loading configuration: %s", err)
-	}
+    config, err := LoadConfig()
+    if err != nil {
+        log.Fatalf("Error loading configuration: %s", err)
+    }
 
-	mail := "From: " + config.GmailUsername + "\n" +
-		"To: " + config.GmailUsername + "\n" +
-		"Subject: Hello there\n\n" +
-		report
+    mail := composeEmail(report, config)
 
-	err = smtp.SendMail(
-		"smtp.gmail.com:587",
-		smtp.PlainAuth("", config.GmailUsername, config.GmailToken, "smtp.gmail.com"),
-		config.GmailUsername,
-		[]string{config.GmailUsername},
-		[]byte(mail),
-	)
+    err = sendEmail(mail, config)
+    if err != nil {
+        log.Printf("Failed to send email: %s", err)
+        return
+    }
 
-	if err != nil {
-		log.Printf("%s", err)
-		return
-	}
-	log.Print("Sent")
+    log.Print("Email sent successfully")
 }
+
+func composeEmail(report string, config *Config) string {
+    return fmt.Sprintf("From: %s\nTo: %s\nSubject: Weekly Pull Request Summary\n\n%s", config.GmailUsername, config.GmailUsername, report)
+}
+
+func sendEmail(mail string, config *Config) error {
+    auth := smtp.PlainAuth("", config.GmailUsername, config.GmailToken, "smtp.gmail.com")
+    err := smtp.SendMail("smtp.gmail.com:587", auth, config.GmailUsername, []string{config.GmailUsername}, []byte(mail))
+    return err
+}
+
