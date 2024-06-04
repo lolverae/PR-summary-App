7e8023a6a0da377e642eb5a1f09fd160f3f47527
lolverae <alberto.olvera.espinosa@gmail.com>
2024-06-04 13:42:23 -0600

    Add functionality for sending the report

--- main.go
+++ main.go
@@ -1,34 +1,62 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"net/smtp"
+	"os"
 
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-    GithubToken   string
-    GmailUsername string
-    GmailToken    string
+	GithubToken   string
+	GmailUsername string
+	GmailToken    string
 }
 
 func LoadConfig() (*Config, error) {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading environment file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading environment file")
+	}
 
-    config := &Config{
-      GithubToken:    os.Getenv("GITHUB_TOKEN"),
-      GmailToken:     os.Getenv("GMAIL_TOKEN"),
-      GmailUsername:  os.Getenv("GMAIL_USERNAME"),
-    }
+	config := &Config{
+		GithubToken:   os.Getenv("GITHUB_TOKEN"),
+		GmailToken:    os.Getenv("GMAIL_TOKEN"),
+		GmailUsername: os.Getenv("GMAIL_USERNAME"),
+	}
 
-    return config, nil
+	return config, nil
 }
 
+func SendReport(report string) {
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %s", err)
+	}
+
+	mail := "From: " + config.GmailUsername + "\n" +
+		"To: " + config.GmailUsername + "\n" +
+		"Subject: Hello there\n\n" +
+		report
+
+	err = smtp.SendMail(
+		"smtp.gmail.com:587",
+		smtp.PlainAuth("", config.GmailUsername, config.GmailToken, "smtp.gmail.com"),
+		config.GmailUsername,
+		[]string{config.GmailUsername},
+		[]byte(mail),
+	)
+
+	if err != nil {
+		log.Printf("%s", err)
+		return
+	}
+	log.Print("Sent")
+}
 
 func main() {
+  report := "Test Report"
+  SendReport(report)
 
 }
