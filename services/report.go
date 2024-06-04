83027e6a700f933a05fef220e6a911b59bd817e7
lolverae <alberto.olvera.espinosa@gmail.com>
2024-06-04 14:13:42 -0600

    Refactor report generation functionality in generateReport

--- services/report.go
+++ services/report.go
@@ -2,7 +2,7 @@ package services
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"log"
 
 	"github.com/google/go-github/github"
@@ -15,21 +15,31 @@ func GenerateReport() string {
 		log.Fatalf("Error loading configuration: %s", err)
 	}
 
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: config.GithubToken},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
+	client, err := createGitHubClient(config.GithubToken)
+	if err != nil {
+		log.Fatalf("Error creating GitHub client: %s", err)
+	}
 
 	githubRepo := "kubernetes"
 	repoOwner := "kubernetes"
 
-	repos, _, err := client.PullRequests.List(ctx, repoOwner, githubRepo, nil)
+	repos, _, err := client.PullRequests.List(context.TODO(), repoOwner, githubRepo, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("Error listing pull requests:", err)
+		return ""
 	}
 
 	return github.Stringify(repos)
+}
+
+func createGitHubClient(token string) (*github.Client, error) {
+	if token == "" {
+		return nil, errors.New("GitHub token is empty")
+	}
+	ctx := context.TODO()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	return github.NewClient(tc), nil
 }
