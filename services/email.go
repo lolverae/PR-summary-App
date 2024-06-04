f90a8b8177f8ce02966cc4fdb712fa8576068503
lolverae <alberto.olvera.espinosa@gmail.com>
2024-06-04 14:17:36 -0600

    Fix formatting

--- services/email.go
+++ services/email.go
@@ -1,35 +1,34 @@
 package services
 
 import (
+	"fmt"
 	"log"
 	"net/smtp"
-  "fmt"
 )
 
 func SendReport(report string) {
-    config, err := LoadConfig()
-    if err != nil {
-        log.Fatalf("Error loading configuration: %s", err)
-    }
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %s", err)
+	}
 
-    mail := composeEmail(report, config)
+	mail := composeEmail(report, config)
 
-    err = sendEmail(mail, config)
-    if err != nil {
-        log.Printf("Failed to send email: %s", err)
-        return
-    }
+	err = sendEmail(mail, config)
+	if err != nil {
+		log.Printf("Failed to send email: %s", err)
+		return
+	}
 
-    log.Print("Email sent successfully")
+	log.Print("Email sent successfully")
 }
 
 func composeEmail(report string, config *Config) string {
-    return fmt.Sprintf("From: %s\nTo: %s\nSubject: Weekly Pull Request Summary\n\n%s", config.GmailUsername, config.GmailUsername, report)
+	return fmt.Sprintf("From: %s\nTo: %s\nSubject: Weekly Pull Request Summary\n\n%s", config.GmailUsername, config.GmailUsername, report)
 }
 
 func sendEmail(mail string, config *Config) error {
-    auth := smtp.PlainAuth("", config.GmailUsername, config.GmailToken, "smtp.gmail.com")
-    err := smtp.SendMail("smtp.gmail.com:587", auth, config.GmailUsername, []string{config.GmailUsername}, []byte(mail))
-    return err
+	auth := smtp.PlainAuth("", config.GmailUsername, config.GmailToken, "smtp.gmail.com")
+	err := smtp.SendMail("smtp.gmail.com:587", auth, config.GmailUsername, []string{config.GmailUsername}, []byte(mail))
+	return err
 }
-
